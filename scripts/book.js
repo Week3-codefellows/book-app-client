5272ab56330b980d1f22729027d335100fb88c56
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-12-19 12:39:22 -0800

    add book-view

--- scripts/book.js
+++ scripts/book.js
@@ -4,6 +4,7 @@ var app = {};
 var __API_URL__ = 'http://localhost:3000';
 //var __API_URL__ = 'https://cs-ea-booklist.herokuapp.com/';
 
+(function(module) {
 function Book(bookObject) {
     Object.keys(bookObject).forEach(key => this[key] = bookObject[key]);
 };
@@ -11,3 +12,20 @@ function Book(bookObject) {
 Book.prototype.toHtml = function() {
     return Handlebars.compile ($('#book-template').text())(this);
 };
+
+Book.all = {};
+
+Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+
+Book.fetchall = callback =>
+    $.get(`${__API_URL__}/tasks`)
+        .then(Book.loadAll)
+        .then(callback)
+        .catch(errorCallback);
+
+module.Book = Book;
+})(app);
+
+$(function() {
+    app.Book.fetchAll(app.bookView.initIndexPage);
+});
