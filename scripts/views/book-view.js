ccab53c29276eb49bd7d765b2703c866fe1f9402
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-12-20 09:30:35 -0800

    bugs

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -1,13 +1,17 @@
-(function() {
+'use strict'
 
-var bookView = {};
-  
-  bookView.initIndexPage = function() {
-      $('.container').hide();
-      $('.book-view').show();
-      app.Book.all.map(book => {
-        $('#book-list').append(book.toHtml())
-    });
-  };
-  Document.ready(app.Book.fetchAll(app.bookView.initIndexPage));
-}());
+var app = app || {};
+
+(function (module) {
+  const bookView = {}
+
+  bookView.initIndexPage = function (err) {
+    $('.container').hide()
+    $('.book-view').show()
+    module.Book.all.map(book => $('#book-list').append(book.toHtml()))
+  }
+
+  module.bookView = bookView
+})(app)
+
+$(() => app.Book.fetchAll(app.bookView.initIndexPage))
--- scripts/views/error-view.js
+++ scripts/views/error-view.js
@@ -1,10 +1,17 @@
-(function() {
+'use strict'
 
-    var errorView = {};
-      
-      errorView.initErrorPage = function(err) {
-          $('.container').hide();
-          $('.error-view').show();
-          // empty error-message
-      };
-    }());
+var app = app || {};
+
+(function(module) {
+  const errorView = {}
+
+  errorView.initErrorView = function(err) {
+    $('.container').hide()
+    $('.error-view').show()
+    $('#error-message').empty()
+    let template = Handlebars.compile($('#error-template').text())
+    $('#error-message').append(template(err))
+  }
+
+  module.errorView = errorView
+})(app)
