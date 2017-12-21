bd217d9601ea2818e82c1ca7a857b06ea330890b
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-12-21 10:51:57 -0800

    finish post

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -3,5 +3,4 @@
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/:id', ctx => app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage));
 page('/books', app.bookView.initFormPage);
-//page('/books', ctx => app.Book.create(ctx, app.bookView.initFormPage));
 page();
